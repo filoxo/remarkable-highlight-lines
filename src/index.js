1104fb671191c201896b6fc5749202723ffd0fbf
Carlos Filoteo <carlos.filoteo@podium.com>
2018-11-19 01:21:37 -0700

    Update index to support remarkable

--- src/index.js
+++ src/index.js
@@ -3,55 +3,39 @@ const RE = /{([\d,-]+)}/
 export default md => {
   const fence = md.renderer.rules.fence
   md.renderer.rules.fence = (...args) => {
-    const [tokens, idx, options, , self] = args
-    const token = tokens[idx]
-
-    if (!token.info || !RE.test(token.info)) {
+    const [tokens, idx, options] = args
+    const {params, content} = tokens[idx]
+    if (!params || !RE.test(params)) {
       return fence(...args)
     }
-
-    const lineNumbers = RE.exec(token.info)[1]
+    const lineNumbers = RE.exec(params)[1]
       .split(',')
       .map(v => v.split('-').map(v => parseInt(v, 10)))
-    const langName = token.info.replace(RE, '').trim()
-
+    const langName = params.replace(RE, '').trim()
     const code = options.highlight ?
-      options.highlight(token.content, langName) :
-      token.content
+      options.highlight(content, langName) :
+      content
     const codeSplits = code.split('\n').map((split, index) => {
       const lineNumber = index + 1
-      const inRange = lineNumbers.some(([start, end]) => {
-        if (start && end) {
-          return lineNumber >= start && lineNumber <= end
-        }
-        return lineNumber === start
-      })
-      if (inRange) {
-        return {
+      const inRange = lineNumbers.some(([start, end]) => start && end
+        ? lineNumber >= start && lineNumber <= end
+        : lineNumber === start
+      )
+      return inRange 
+        ? {
           code: `<span class="highlighted-line">${split}</span>`,
           highlighted: true
         }
-      }
-      return {
-        code: split
-      }
+        : {code: split}
     })
-    let highlightedCode = ''
-    codeSplits.forEach(split => {
-      if (split.highlighted) {
-        highlightedCode += split.code
-      } else {
-        highlightedCode += `${split.code}\n`
-      }
-    })
+    let highlightedCode = codeSplits.reduce((code, split) => {
+      code += split.highlighted ? split.code : `${split.code}\n`
+      return code
+    }, '')
     // If custom highlighter wraps code with starting <pre..., don't wrap code
     if (highlightedCode.startsWith('<pre')) {
       return highlightedCode
     }
-    const tmpToken = {
-      attrs: [['class', langName ? `language-${langName}` : '']]
-    }
-    const attrs = self.renderAttrs(tmpToken)
-    return `<pre${attrs}><code${attrs}>${highlightedCode.trim()}</code></pre>`
+    return `<pre><code ${langName ? `class="${options.langPrefix}${langName}"` : ''}>${highlightedCode.trim()}</code></pre>`
   }
 }
